017d474f839dee6ba50b370705fa3295cdf5463d
Ikram <syed.khaja.ikramuddin9@gmail.com>
2023-01-20 17:20:27 -0500

    lab2 files

--- Week2/index.js
+++ Week2/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Goodbye World');
+  res.end('Goodbyeee World');
 });
 
 server.listen(port, hostname, () => {
